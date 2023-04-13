44efdd1020aaf82f609cbb258dd5f50ba42c3b37
mu-837 <1282807292@qq.com>
2023-04-14 00:31:56 +0800

    最小队列，发现之前的最大队列的bug

--- VSCode/DataStructure/Queue.h
+++ VSCode/DataStructure/Queue.h
@@ -13,7 +13,10 @@ using namespace std;
 // max_value, push_back, pop_front的均摊复杂度均为O(1)
 // 其他两个好分析，push_back中有从尾部pop所有小于value的值得操作，但pop操作最多执行次数和push_back次数相同，所以均摊到每个元素上还是O(1);
 // 最小值同理；
+// ! MaxQueue发现bug，在同时入队连续的相同值时，dq_max并没有重复入队，会导致一些问题；请见cpp main中的测试用例；不知道这个代码当初是如何通过的；
+// 下面的MinQueue无此bug, 要修复MaxQueue的此bug，只需在value和dq_max.back相同时也压入value即可；
 class MaxQueue {
+    // ! 未修复bug，不要使用；
     queue<int> q;
     deque<int> dq_max;
 public:
@@ -53,4 +56,37 @@ public:
     }
 };
 
-// TODO: 最小队列
+// 最小队列，等价于上面队列的最大值；
+class MinQueue {
+    queue<int> q;
+    deque<int> dq_min;
+    // q即队列，dq_min维护最小值；
+    // 入队一个小值时会导致之前入队的值在最小值中失效；（被更小值覆盖）
+    // 入队一个大值时会在之前入队的小值出队时生效；
+    // 出队的值一定大于等于dq_min记录的值，相等时说明最小值以出队，所以dq_min也出队；
+public:
+    MinQueue() {
+
+    }
+    int min_value() {
+        return dq_min.front();
+    }
+    void push_back(int value) {
+        q.push(value);
+        while(!dq_min.empty() && value<dq_min.back()) {
+            dq_min.pop_back(); // 被小值覆盖；
+        }
+        dq_min.push_back(value);
+    }
+    int pop_front() {
+        if(q.empty())
+            return -1;
+        
+        int temp = q.front();
+        q.pop();
+
+        if(temp == dq_min.front())
+            dq_min.pop_front();
+        return temp;
+    }
+};
--- VSCode/queue.cpp
+++ VSCode/queue.cpp
@@ -8,13 +8,34 @@ using namespace std;
 
 int main()
 {
-    cout<<"最大队列"<<endl;
+    cout<<"最大队列With Bug"<<endl;
     MaxQueue mq;
     mq.push_back(1);
+    mq.push_back(2);
+    mq.push_back(2);
     mq.push_back(2);
     cout<<mq.max_value()<<endl; // 2
     cout<<mq.pop_front()<<endl; // 1
     cout<<mq.max_value()<<endl; // 2
+    cout<<mq.pop_front()<<endl; // 2
+    cout<<mq.max_value()<<endl; // 应该为2
+    cout<<mq.pop_front()<<endl; // 2
+    cout<<mq.max_value()<<endl; // 应该为2
+
+    cout<<"最小队列"<<endl;
+    MinQueue minq;
+    minq.push_back(2);
+    minq.push_back(1);
+    minq.push_back(1);
+    minq.push_back(1);
+    cout<<minq.min_value()<<endl; // 1
+    cout<<minq.pop_front()<<endl; // 2
+    cout<<minq.min_value()<<endl; // 1
+    cout<<minq.pop_front()<<endl; // 1
+    cout<<minq.min_value()<<endl; // 1
+    cout<<minq.pop_front()<<endl; // 1
+    cout<<minq.min_value()<<endl; // 1
+
 
     
     return 0;
